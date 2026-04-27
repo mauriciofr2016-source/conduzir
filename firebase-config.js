73e6a198032a8f33bc62d1d358bf4f4a635465c2
mauriciofr2016-source <mauriciofr2016@gmail.com>
2026-04-27 10:01:29 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.BT_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBfvvueEZeFA5jxL_AZDf977M_syqloJrY",
-  authDomain: "bancotalentoserika.firebaseapp.com",
-  projectId: "bancotalentoserika",
-  storageBucket: "bancotalentoserika.firebasestorage.app",
-  messagingSenderId: "880124571423",
-  appId: "1:880124571423:web:516695982e0af05fda8a2a"
-};
